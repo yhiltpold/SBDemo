9b0198a7edf7726800a313f9496fe1368ac62d88
Yves Hiltpold <yves@yvess-macbook-pro.home>
2022-10-31 22:32:43 +0100

    Initial commit

---
+++ src/test/java/org/hiltpold/sbdemo/SbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hiltpold.sbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
